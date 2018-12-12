0f9315837c7dbe4166f72ca46e6791eefa67ae42
shilpipawar <shilpipawar@gmail.com>
2018-12-12 00:52:52 -0600

    Added Inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,20 +10,61 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    allSongByArtist();
+    allProductsforSale();
+    //buyProduct();
     cleanUp();
 });
 
-function allSongByArtist() {
-    // var sql = "SELECT * FROM album where artist = ?";
-    // var values = ;
-     connection.query("SELECT * FROM products", function (err, result, fields) {
-    //connection.query(sql, [values], function (err, result) {
+function allProductsforSale() {
+    connection.query("SELECT * FROM products", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
+        buyProduct();
     });
 }
 
 function cleanUp() {
     connection.end();
+}
+function placeOredre(id, quantity) {
+
+}
+function checkProductQuantity(id) {
+
+}
+//The app should then prompt users with two messages.
+//The first should ask them the ID of the product they would like to buy.
+//The second message should ask how many units of the product they would like to buy.
+function validateProductId(name) {
+    //return name !== '';
+    if (name == parseInt(name)) {
+        return name !== '';
+    }
+}
+
+function buyProduct() {
+    inquirer
+        .prompt([
+            // Here we create a basic text prompt.
+            {
+                type: "input",
+                message: "Enter Product Key:",
+                name: "pid",
+                validate: validateProductId
+            },
+            {
+                type: "input",
+                message: "How many quantity you want to buy:",
+                name: "quantity",
+                validate: validateProductId
+            }, {
+                type: "confirm",
+                message: "Are you sure:",
+                name: "confirm",
+                default: true
+            }
+        ])
+        .then(function (inquirerResponse) {
+
+        });
 }
