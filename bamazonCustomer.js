c35eb0a00304fb0d360141e8100335b97871de42
shilpipawar <shilpipawar@gmail.com>
2018-12-15 00:25:21 -0600

    changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,8 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     allProductsforSale();
     //buyProduct();
-    cleanUp();
+    console.log("calling CLeanup");
+
 });
 
 function allProductsforSale() {
@@ -26,11 +27,61 @@ function allProductsforSale() {
 function cleanUp() {
     connection.end();
 }
-function placeOredre(id, quantity) {
-
+function placeOredre(id, enteredQty, dbQty) {
+    inquirer
+        .prompt([
+            {
+                type: "confirm",
+                message: "Are you sure to place the order:",
+                name: "confirm",
+                default: true
+            }
+        ])
+        .then(function (response) {
+           
+            if (response.confirm) {
+                 var updateQty = parseInt(dbQty) - parseInt(enteredQty)
+                connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [updateQty, id], function (error, results, fields) {
+                    if (error) {
+                      console.log("error", error);
+                    }
+                    console.log("Order placed successfully!! Thanks you");
+                  });
+               
+                // var sql = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+                // var values = [updateQty, id];
+                // connection.query(sql, [values], function (err, result, fields) {
+                //     if (err) throw err;
+    
+                //     console.log("Qyantity:" + id + " Qty:" + enteredQty);
+    
+                //     console.log("working");
+                // });
+                // console.log("Order Places!!!");
+            }
+            else {
+                console.log("\nThat's okay ");
+            }
+            cleanUp();
+        });
 }
-function checkProductQuantity(id) {
 
+function checkProductQuantity(id, quantity) {
+    var sql = "select stock_quantity from products where item_id = ?";
+    var values = id;
+    connection.query(sql, [values], function (err, result, fields) {
+        if (err) throw err;
+
+        console.log("Qyantity:" + result[0].stock_quantity + " Qty:" + quantity);
+
+        var dbQty = parseInt(result[0].stock_quantity);
+        var enteredQty = parseInt(quantity);
+        if (enteredQty <= dbQty) {
+            console.log("Hello");
+            placeOredre(id, enteredQty, dbQty);
+        }
+        console.log("not working");
+    });
 }
 //The app should then prompt users with two messages.
 //The first should ask them the ID of the product they would like to buy.
@@ -41,7 +92,6 @@ function validateProductId(name) {
         return name !== '';
     }
 }
-
 function buyProduct() {
     inquirer
         .prompt([
@@ -57,14 +107,10 @@ function buyProduct() {
                 message: "How many quantity you want to buy:",
                 name: "quantity",
                 validate: validateProductId
-            }, {
-                type: "confirm",
-                message: "Are you sure:",
-                name: "confirm",
-                default: true
             }
         ])
         .then(function (inquirerResponse) {
-
+            console.log(inquirerResponse.pid + " " + inquirerResponse.quantity);
+            checkProductQuantity(inquirerResponse.pid, inquirerResponse.quantity);
         });
 }
