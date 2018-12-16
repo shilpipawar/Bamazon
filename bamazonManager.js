c63a301a5b74daf4bdffbd1679a105700f082a39
shilpipawar <shilpipawar@gmail.com>
2018-12-15 23:30:46 -0600

    Manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,24 +15,53 @@ connection.connect(function (err) {
     console.log("calling CLeanup");
 });
 ////////DB Function/////////////////////////////////////////////////
-function productSale(){
-
+function productSale() {
+    connection.query("select item_id,product_name,price,stock_quantity from products where stock_quantity > 0", function (err, result, fields) {
+        if (err) throw err;
+        const table = cTable.getTable(result);
+        console.log(table);
+    });
 }
-function lowInventory(){
-
+function lowInventory() {
+    connection.query("select item_id,product_name,price,stock_quantity from products where stock_quantity < 50", function (err, result, fields) {
+        if (err) throw err;
+        const table = cTable.getTable(result);
+        console.log(table);
+    });
 }
-function addInventory(){
+function addInventory() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter Product id:",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "Add Quantity:",
+                name: "quantity",
+            }
+        ])
+        .then(function (response) {
+            connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [response.quantity, response.id], function (error, results, fields) {
+                if (error) {
+                    console.log("error", error);
+                }
+                console.log("Product Quantity Updated!!");
 
+            });
+        });
 }
-function addProduct(){
-    
+function addProduct() {
+
 }
 ////////////////////////////////Manager View//////////////////////////////////////////////
 function managerOptions() {
     inquirer
         .prompt([
             {
-                message: "List a set of menu options",
+                message: "Menu options - Manager View",
                 type: "checkbox",
                 name: "list",
                 choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
@@ -41,14 +71,16 @@ function managerOptions() {
 
             if (response.list[0] == "View Products for Sale") {
                 productSale();
-            } else if (response.list[1] == "View Low Inventory") {
-                lowInventory();
-            } else if (response.list[2] == "Add to Inventory") {
-                addInventory();
-            } else if (response.list[3] == "Add New Product") {
-                addProduct();
-            } else {
-                console.log("No Option Selected");
             }
+            if (response.list[0] == "View Low Inventory") {
+                lowInventory();
+            }
+            if (response.list[0] == "Add to Inventory") {
+                addInventory();
+            }
+            if (response.list[0] == "Add New Product") {
+                addProduct();
+            }
+            //console.log("No Option Selected" + response.list);
         });
 }
